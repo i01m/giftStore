56519036256e639beb84510b1927856a7d3179b0
Mister <i.p.m.a.n.03.31.87@gmail.com>
2025-01-13 22:57:59 -0500

    Add files via upload

---
+++ GiftsForEveryone/Startup.cs
@@ -0,0 +1,107 @@
+﻿using GiftsForEveryone.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GiftsForEveryone
+{
+    /*
+     Helpful Notes
+    - To use this program on new VS (new pc), need to create local databases (see below),
+      Local DB component needs to be installed in that ASP.NET Visual Studio.
+    - to Seed Database go to admin access  /Admin/Index (seed option is only available is there are no products)
+    - adminUser = "Admin"adminPassword = "Secret123$" (located in the Model/IdentitySeedData.cs
+    - view unshipped list of orders, Order/List
+    - drop database via powershell: dotnet ef database drop --force --context ApplicationDbContext
+    - build db back: dotnet ef database update --context ApplicationDbContextdotnet             
+     */
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration["Data:SportStoreProducts:ConnectionString"]));
+
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration["Data:SportStoreIdentity:ConnectionString"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IOrderRepository,EFOrderRepository>();
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+
+            //commented below is for Azure deployment
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseStatusCodePages();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //}
+
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseAuthentication();
+            app.UseMvc(routes => {
+                routes.MapRoute(name: "Error", template: "Error",
+                    defaults: new { controller = "Error", action = "Error" });
+
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}/Page{productPage:int}",
+                    defaults: new { controller = "Product", action = "List" }
+                );
+
+                routes.MapRoute(
+                    name: null,
+                    template: "Page{productPage:int}",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 }
+                );
+
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 }
+                );
+
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 });
+
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
+            });
+
+            //SeedData.EnsurePopulated(app);
+            //IdentitySeedData.EnsurePopulated(app);
+        }
+    }
+}
