34eae18f2ba543d83f03298e2c06a085547cfa77
Mister <i.p.m.a.n.03.31.87@gmail.com>
2025-01-13 23:02:17 -0500

    Add files via upload

---
+++ GiftsForEveryone/bower_components/manualProductsInsertSql.sql
@@ -0,0 +1,21 @@
+﻿select * from Products
+
+--insert into Products (Category,Description, Name,Price)
+--values ('Watersports','A boat for one person','Kayak',275)
+--insert into Products (Category,Description, Name,Price)
+--values ('Watersports','Protective and fashionable','Lifejacket',48.95)
+--insert into Products (Category,Description, Name,Price)
+--values ('Soccer','FIFA-approved size and weight','Soccer Ball',19.50)
+--insert into Products (Category,Description, Name,Price)
+--values ('Soccer','Give your playing field a professional touch','Corner Flags',34.95)
+--insert into Products (Category,Description, Name,Price)
+--values ('Soccer','Flat-packed 35,000-seat stadium','Stadium',79500)
+--insert into Products (Category,Description, Name,Price)
+--values ('Chess','Improve brain efficiency by 75%','Thinking Cap',16)
+--insert into Products (Category,Description, Name,Price)
+--values ('Chess','Secretly give your opponent a disadvantage','Unsteady Chair',29.95)
+--insert into Products (Category,Description, Name,Price)
+--values ('Chess','A fun game for the family','Human Chess Board',75)
+--insert into Products (Category,Description, Name,Price)
+--values ('Chess','Gold-plated, diamond-studded King','Bling-Bling King',1200)
+
